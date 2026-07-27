dc243fb9e4f8ec8ab3c1f990b22f60395b4090f7
prachi447 <prachiupadhyay112233@gmail.com>
2026-07-27 19:36:52 +0530

    Create songs.js with song database
    Added a song database with details for multiple songs.

---
+++ songs.js
@@ -0,0 +1,47 @@
+// ==========================================
+// TEHALKA MUSIC - SONG DATABASE
+// ==========================================
+
+const songsData = [
+
+  {
+    name: "Kamar Kamar",
+    artist: "Billa Sonipat Ala",
+    image: "Kamar-Kamar-Unknown-2025-20251126103334-500x500.jpg",
+    mp3: "Kamar_Kamar_1.mp3",
+    page: "kamar-kamar.html"
+  },
+
+  {
+    name: "Parindey",
+    artist: "Sumit Goswami",
+    image: "size_m.jpg",
+    mp3: "Parindey_1.mp3",
+    page: "parindey.html"
+  },
+
+  {
+    name: "Sukoon",
+    artist: "Oye Gurmeet & Prachi Upadhyay",
+    image: "Sukoon.png",
+    mp3: "Sukoon.mp3",
+    page: "Sukoon.html"
+  },
+
+  {
+    name: "Chundadi",
+    artist: "Shiqari",
+    image: "hqdefault.jpg",
+    mp3: "Chundadi.mp3",
+    page: "Chundadi.html"
+  },
+
+  {
+    name: "Patli Kamar",
+    artist: "Mohit Sharma",
+    image: "Patali kamar.jpg",
+    mp3: "Patli_Kamar_1.mp3",
+    page: "patli-kamar.html"
+  }
+
+];
