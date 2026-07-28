ac292032e562956429a6b1ea96faa46dec37f18c
prachi447 <prachiupadhyay112233@gmail.com>
2026-07-28 10:34:26 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -44,12 +44,4 @@ const songsData = [
     page: "patli-kamar.html"
   }
 
-
-  {
-    name: "Test Song",
-    artist: "Test Artist",
-    image: "test.jpg",
-    mp3: "test.mp3",
-    page: "test-song.html"
-}
-];
+]
