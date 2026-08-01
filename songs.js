d7e7135837c92ac6be568cd87bf8c69440e3429f
prachi447 <prachiupadhyay112233@gmail.com>
2026-08-01 17:45:07 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -7,41 +7,56 @@ const songsData = [
   {
     name: "Kamar Kamar",
     artist: "Billa Sonipat Ala",
+    album: "Single",
+    category: "Haryanvi",
     image: "Kamar-Kamar-Unknown-2025-20251126103334-500x500.jpg",
     mp3: "Kamar_Kamar_1.mp3",
-    page: "kamar-kamar.html"
+    page: "kamar-kamar.html",
+    description: "Listen to Kamar Kamar by Billa Sonipat Ala on Tehalka Music."
   },
 
   {
     name: "Parindey",
     artist: "Sumit Goswami",
+    album: "Single",
+    category: "Haryanvi",
     image: "size_m.jpg",
     mp3: "Parindey_1.mp3",
-    page: "parindey.html"
+    page: "parindey.html",
+    description: "Listen to Parindey by Sumit Goswami on Tehalka Music."
   },
 
   {
     name: "Sukoon",
     artist: "Oye Gurmeet & Prachi Upadhyay",
+    album: "Single",
+    category: "Hindi",
     image: "Sukoon.png",
     mp3: "Sukoon.mp3",
-    page: "sukoon.html"
+    page: "sukoon.html",
+    description: "Listen to Sukoon by Oye Gurmeet & Prachi Upadhyay on Tehalka Music."
   },
 
   {
     name: "Chundadi",
     artist: "Shiqari",
+    album: "Single",
+    category: "Haryanvi",
     image: "hqdefault.jpg",
     mp3: "Chundadi.mp3",
-    page: "Chundadi.html"
+    page: "Chundadi.html",
+    description: "Listen to Chundadi by Shiqari on Tehalka Music."
   },
 
   {
     name: "Patli Kamar",
     artist: "Mohit Sharma",
+    album: "Single",
+    category: "Haryanvi",
     image: "Patali kamar.jpg",
     mp3: "Patli_Kamar_1.mp3",
-    page: "patli-kamar.html"
+    page: "patli-kamar.html",
+    description: "Listen to Patli Kamar by Mohit Sharma on Tehalka Music."
   },
 
   {
