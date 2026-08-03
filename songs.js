e0497e126bdea0ee00cdebedea39e3e99a0333cc
prachi447 <prachiupadhyay112233@gmail.com>
2026-08-03 22:25:10 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -7,57 +7,47 @@ const songsData = [
   {
     name: "Kamar Kamar",
     artist: "Billa Sonipat Ala",
-    album: "Single",
-    category: "Haryanvi",
     image: "Kamar-Kamar-Unknown-2025-20251126103334-500x500.jpg",
     mp3: "Kamar_Kamar_1.mp3",
-    page: "kamar-kamar.html",
-    description: "Listen to Kamar Kamar by Billa Sonipat Ala on Tehalka Music."
+    page: "kamar-kamar.html"
   },
+
 
   {
     name: "Parindey",
     artist: "Sumit Goswami",
-    album: "Single",
-    category: "Haryanvi",
     image: "size_m.jpg",
     mp3: "Parindey_1.mp3",
-    page: "parindey.html",
-    description: "Listen to Parindey by Sumit Goswami on Tehalka Music."
+    page: "parindey.html"
   },
+
 
   {
     name: "Sukoon",
     artist: "Oye Gurmeet & Prachi Upadhyay",
-    album: "Single",
-    category: "Hindi",
     image: "Sukoon.png",
     mp3: "Sukoon.mp3",
-    page: "sukoon.html",
-    description: "Listen to Sukoon by Oye Gurmeet & Prachi Upadhyay on Tehalka Music."
+    page: "sukoon.html"
   },
+
 
   {
     name: "Chundadi",
     artist: "Shiqari",
-    album: "Single",
-    category: "Haryanvi",
     image: "hqdefault.jpg",
     mp3: "Chundadi.mp3",
-    page: "Chundadi.html",
-    description: "Listen to Chundadi by Shiqari on Tehalka Music."
+    page: "Chundadi.html"
   },
+
 
   {
     name: "Patli Kamar",
     artist: "Mohit Sharma",
-    album: "Single",
-    category: "Haryanvi",
     image: "Patali kamar.jpg",
     mp3: "Patli_Kamar_1.mp3",
-    page: "patli-kamar.html",
-    description: "Listen to Patli Kamar by Mohit Sharma on Tehalka Music."
+    page: "patli-kamar.html"
   },
+
 
   {
     name: "Ghadi",
@@ -67,7 +57,21 @@ const songsData = [
     image: "sddefault.jpg",
     mp3: "Ghadi.mp3",
     page: "ghadi.html",
-    description: "Listen to Ghadi by Mitta Bahu Ala, a Haryanvi song on Tehalka Music."
+    description:
+      "Listen to Ghadi by Mitta Bahu Ala, a Haryanvi song on Tehalka Music."
+  },
+
+
+  {
+    name: "Main Teri Nachai Nachun",
+    artist: "Raaj Mawar, Rammehar Mehla",
+    album: "Singles",
+    category: "Haryanvi",
+    image: "Cover.jpg",
+    mp3: "Main Teri Nachai Nachun.mp3",
+    page: "main-teri-nachai-nachun.html",
+    description:
+      "Listen to Main Teri Nachai Nachun by Raaj Mawar and Rammehar Mehla on Tehalka Music."
   }
 
 ];
