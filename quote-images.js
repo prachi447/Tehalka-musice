1afad71093d070984c7f36c1460b06503c5fe8f8
prachi447 <prachiupadhyay112233@gmail.com>
2026-08-11 22:56:49 +0530

    Create quote-images.js

---
+++ quote-images.js
@@ -0,0 +1,19 @@
+const quoteImages = [
+
+    {
+        id: 1,
+
+        quoteId: 9,
+
+        title: "नई शुरुआत",
+
+        category: "Life",
+
+        text: "हर दिन एक नई शुरुआत है,\nबस खुद पर विश्वास होना चाहिए।",
+
+        author: "Tehalka Quotes",
+
+        image: "images/quotes/nayi-shuruaat.png"
+    }
+
+];
